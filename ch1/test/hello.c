7cd3aad254367d2f5ab0848d6372bd84cde9d1cc
zhenkai13602854966 <13602854966@163.com>
2019-09-06 10:20:05 +0800

    first commit

---
+++ ch1/test/hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: zhenkai
+	> Mail: 996.icu.cn@gmail.com 
+	> Created Time: 五  9/ 6 10:16:57 2019
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello world!\n");
+	return 0;
+}
